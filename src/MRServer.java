7187934015859a47df0dadadaccba136803c1a57
YingJie-Zhao <YingJie-Zhao@outlook.com>
2021-06-29 15:49:38 +0800

    Add combine function(to verify if MapReduce function appropriately executed)

--- src/MRServer.java
+++ src/MRServer.java
@@ -1,12 +1,17 @@
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.rmi.AlreadyBoundException;
 import java.rmi.Naming;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.RMISocketFactory;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -41,8 +46,10 @@ public class MRServer {
         while (true) {
             if (master.done()) {
                 long end = System.currentTimeMillis();
-                double cost = (double) (end - begin) / 60000;
-                System.out.println("RMI server stopped.Total cost time(in minutes): " + "%.2f".formatted(cost));
+                double cost = (double) (end - begin) / 1000;
+                System.out.println("RMI server stopped.Total cost: " + "%.2f seconds".formatted(cost));
+                // combine mr-out- files into one file,can be used to verify if MapReduce function appropriately executed
+                //combine();
                 System.exit(0);
             }
             Thread.sleep(2000);
@@ -57,5 +64,39 @@ public class MRServer {
             System.exit(1);
         }
         return Arrays.stream(files).map(File::getName).collect(Collectors.toList());
+    }
+
+    private static void combine() throws IOException {
+        File dir = new File("./");
+        File[] files = dir.listFiles();
+        if (null == files) {
+            System.out.println("No files found!");
+            System.exit(1);
+        }
+        files = Arrays.stream(files).filter(f -> f.getName().startsWith("mr-out-")).toArray(File[]::new);
+        List<KeyValue> intermediate = new LinkedList<>();
+        for (File file : files) {
+            try (FileInputStream fs = new FileInputStream(file)) {
+                byte[] data = new byte[(int) file.length()];
+                fs.read(data);
+                String[] lines = new String(data, StandardCharsets.UTF_8).split("\n");
+                for (String line : lines) {
+                    String[] content = line.split(" ");
+                    intermediate.add(new KeyValue(content[0], content[1]));
+                }
+            } catch (IOException e) {
+                System.out.println("File not found: " + file);
+                System.exit(1);
+            }
+        }
+
+        Collections.sort(intermediate);
+        File oFile = new File("combine-out");
+        FileWriter fw = new FileWriter(oFile, true);
+        for (KeyValue keyValue : intermediate) {
+            String content = "%s %s\n".formatted(keyValue.key, keyValue.value);
+            fw.append(content);
+        }
+        fw.close();
     }
 }
